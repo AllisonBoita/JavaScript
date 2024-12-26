b302337d3f974afc6f0b5186093ed62f562c13f8
Allison Cristiano Boita <allison.boita75@gmail.com>
2024-12-26 11:58:49 -0300

    Operadores de Atribuição

--- Dia 06 - Calculos Matemáticos/Dia 05 - Linguagens Estatísticas ou Dinâmicas/script.js
+++ Dia 06 - Calculos Matemáticos/Dia 05 - Linguagens Estatísticas ou Dinâmicas/script.js
@@ -7,9 +7,20 @@ total = total / 3 // Divisao
 console.log(total) */
 
 
-let total = 6 + 6
+/* let total = 6 + 6
 total = total % 5 // Resto da divisão
 total++ // Incremento
 total-- // Decremento
 total = total ** 3 // Exponenciação
+console.log(total) */
+
+// Operadores de Atribuição
+
+let total = 3
+total += 5 // Operador de atribuição
+total -= 3 // Subtração
+total *= 5 // Multiplicação
+total /= 3 // Divisão
+total %= 2 // Resto
+total **= 3 // Exponenciação
 console.log(total)
