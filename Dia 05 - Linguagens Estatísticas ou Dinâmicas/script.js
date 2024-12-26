53d2549b1803fe55a855359150cc7bd3256f458a
Allison Cristiano Boita <allison.boita75@gmail.com>
2024-12-26 11:32:05 -0300

    Convertendo String para Numero

---
+++ Dia 05 - Linguagens Estatísticas ou Dinâmicas/script.js
@@ -0,0 +1,42 @@
+// Static ou Dynamic
+
+// Static: C, C++, Java, Typescript
+
+// Dynamic: Python, JavaScript, Ruby, PHP
+
+// O javascript consegue detectar o tipo de dado e associar à variável.
+
+/* let firstName = 'Allison'
+let age = 35
+ */
+
+
+// Convertions ou Casting
+
+// ParseInt = (Inteiro)
+
+/* let age = '34'
+console.log(age, typeof age)
+age = parseInt(age)
+console.log(age, typeof age) */
+
+// ParseFloat = (Fracionario)
+
+/* let age = '34.2'
+console.log(age, typeof age)
+age = parseFloat(age)
+console.log(age, typeof age)
+ */
+
+// Unário (+)
+
+/* let age = '34.2'
+age = +age
+console.log(age, typeof age) */
+
+// Number (numero)
+
+/* let age = '35'
+age = Number(age)
+console.log(age, typeof age)
+ */
