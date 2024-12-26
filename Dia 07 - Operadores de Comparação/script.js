e94faaff5882ee4c5800a3c4c644dc8e70e1d56e
Allison Cristiano Boita <allison.boita75@gmail.com>
2024-12-26 16:55:26 -0300

    Comparação de Maior e Menor

--- Dia 07 - Operadores de Comparação/script.js
+++ Dia 07 - Operadores de Comparação/script.js
@@ -16,3 +16,10 @@ console.log(3 != 3)
 // Desigualdade Estrita (tipo do dado)
 
 console.log('3' !== 3)
+
+// Maior que
+
+console.log(3 > 2) // Maior que
+console.log(3 >= 3) // Maior ou Igual que
+console.log(3 < 2) // Menor que
+console.log(3 <= 3) // Menor ou Igual que
