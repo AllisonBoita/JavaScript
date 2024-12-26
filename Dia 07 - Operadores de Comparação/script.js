584935d4e81f64d32c3cf8ac8d8ab40226d58159
Allison Cristiano Boita <allison.boita75@gmail.com>
2024-12-26 16:52:40 -0300

    Dia 07: Operadores de Comparação

---
+++ Dia 07 - Operadores de Comparação/script.js
@@ -0,0 +1,18 @@
+// Operadores Comparação
+
+// Igualdade Solta (dado)
+
+console.log(3 == 3)
+
+// Igualdade Estrita (tipo do dado)
+
+console.log('2' === 2)
+console.log(2 === 2)
+
+// Desigualdade Solta (dado)
+
+console.log(3 != 3)
+
+// Desigualdade Estrita (tipo do dado)
+
+console.log('3' !== 3)
