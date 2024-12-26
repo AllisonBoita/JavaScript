7b47805084802f32ccf544dd1efb963f19e7ec6e
Allison Cristiano Boita <allison.boita75@gmail.com>
2024-12-26 11:54:36 -0300

    Dia 06: Operadores Aritméticos

--- Dia 06 - Calculos Matemáticos/Dia 05 - Linguagens Estatísticas ou Dinâmicas/script.js
+++ Dia 06 - Calculos Matemáticos/Dia 05 - Linguagens Estatísticas ou Dinâmicas/script.js
@@ -1,8 +1,15 @@
 // Operadores Aritméticos Simples
 
-let total = 6 + 6 // Adição
+/* let total = 6 + 6 // Adição
 total = total - 4 // Subtração
 total = total * 2 // Multiplicação
 total = total / 3 // Divisao
-console.log(total)
+console.log(total) */
 
+
+let total = 6 + 6
+total = total % 5 // Resto da divisão
+total++ // Incremento
+total-- // Decremento
+total = total ** 3 // Exponenciação
+console.log(total)
