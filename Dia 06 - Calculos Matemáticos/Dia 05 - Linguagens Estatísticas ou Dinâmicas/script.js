1687ee80d6694a8a63fec49a33e391e6ad3ddc20
Allison Cristiano Boita <allison.boita75@gmail.com>
2024-12-26 11:50:24 -0300

    Dia 06: Calculos Matematicos

---
+++ Dia 06 - Calculos Matemáticos/Dia 05 - Linguagens Estatísticas ou Dinâmicas/script.js
@@ -0,0 +1,8 @@
+// Operadores Aritméticos Simples
+
+let total = 6 + 6 // Adição
+total = total - 4 // Subtração
+total = total * 2 // Multiplicação
+total = total / 3 // Divisao
+console.log(total)
+
