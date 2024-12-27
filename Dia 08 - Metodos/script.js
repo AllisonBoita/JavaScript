2230d7bfe55c92274af4fe1e388b236b08339eb4
Allison Cristiano Boita <allison.boita75@gmail.com>
2024-12-27 09:57:11 -0300

    Dia 08: Template Literals e Metodos

---
+++ Dia 08 - Metodos/script.js
@@ -0,0 +1,20 @@
+// Template Literals
+
+/* let firstName = 'Allison'
+let lastName = 'Boita'
+let age = 32 
+
+console.log(`Olá meu nome é ${firstName} ${lastName} e eu tenho ${age} anos de idade`)
+
+*/
+
+// String Methods
+
+let texto = 'Estou aprendendo Javascript'
+console.log(texto.includes('ap')) // verifica se o trecho existe
+console.log(texto.charAt(4)) // A letra desta posição
+console.log(texto.indexOf('Jav')) // Onde o trecho inicia
+console.log(texto.slice(6,16)) // Recorta ou separa o trecho
+console.log(texto.toUpperCase()) // Tudo para letras maiusculas
+console.log(texto.toLowerCase()) // Tudo para letras minusculas
+console.log(texto.trim())
