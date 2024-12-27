e93f2514814f9f10916daa3f6275d2fcac9008f5
Allison Cristiano Boita <allison.boita75@gmail.com>
2024-12-27 10:30:01 -0300

    Dia 08: Metodos Matemáticos

--- Dia 08 - Metodos/script.js
+++ Dia 08 - Metodos/script.js
@@ -24,7 +24,19 @@ console.log(texto.replace('Estou', 'Continuo')) // Trocando texto
 
 // Number Methods
 
-let num1 = 3.23
+/* let num1 = 3.23
 console.log(num1)
 console.log(num1.toFixed(1)) // Definir a quantidade de números após a vírgula.
 console.log(num1.toString(3)) // Posso transformar em string e passar uma base por exemplo (Binario)
+ */
+
+// Metodos Matematicos (Math)
+
+let num1 = 16
+console.log(Math.round(num1)) // Transforma o número fracionário em inteiro
+console.log(Math.ceil(num1)) // Com o ceil ele sempre arredonda pra cima.
+console.log(Math.floor(num1)) // Com o floor ele sempre arredonda pra baixo.
+console.log(Math.sqrt(num1)) // Raiz quadrada
+console.log(Math.pow(num1, 3)) // Calculo de potencia
+console.log(Math.abs(num1, 3)) // Valor absoluto
+console.log(Math.round(Math.random() * 10 + 1)) // Gerando numero randomico
