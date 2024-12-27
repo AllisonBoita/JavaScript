04160a5cd6e4d1374f25c79c79b978586596cd00
Allison Cristiano Boita <allison.boita75@gmail.com>
2024-12-27 11:26:55 -0300

    Dia 09: Os metodos do Date

--- Dia 09 - Date/script.js
+++ Dia 09 - Date/script.js
@@ -10,3 +10,10 @@ let dataString =  new Date('2024/10/20 10:35:00')
 console.log(dataString)
 
 // Os metodos do Date
+
+let data = new Date()
+console.log(data.getDate()) // Pegar dia do mês
+console.log(data.getDay()) // Dia da semana (0 é domingo) 
+console.log(data.getMonth()) // Mês (Janeiro inicia em 0)
+console.log(data.getFullYear()) // Ano
+console.log(data.getHours()) // Horas
