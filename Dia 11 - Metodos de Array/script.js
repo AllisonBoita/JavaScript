20f80fbf22d1223acce6cf01e4e6565241bcde57
Allison Boita <allison.boita75@gmail.com>
2024-12-28 12:44:21 -0300

    Metodos de uma Array

---
+++ Dia 11 - Metodos de Array/script.js
@@ -0,0 +1,9 @@
+// Arrays - Estrutura de dados que permite armazenar um conjunto de dados.
+
+let cart = ['maca', 'laranja', 'agua', 'arroz']
+//cart.push = 'Pera' // Adiciona item ao fim do array
+//cart.pop() // Remove o ultimo item da lista
+//cart.shift() // Remove o primeiro item da lista
+//cart.unshift('Suco') // Adiciona o elemento ao inicio da array
+//cart.sort() // Ele organiza os elementos em ordem alfabetica
+console.log(cart) // Descobrir o tamanho da array
