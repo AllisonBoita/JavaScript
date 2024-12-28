24d56c09c2f8236460a1326a796d090bf0f14881
Allison Boita <allison.boita75@gmail.com>
2024-12-28 12:51:08 -0300

    Metodos Slice e Splice

--- Dia 11 - Metodos de Array/script.js
+++ Dia 11 - Metodos de Array/script.js
@@ -6,4 +6,13 @@ let cart = ['maca', 'laranja', 'agua', 'arroz']
 //cart.shift() // Remove o primeiro item da lista
 //cart.unshift('Suco') // Adiciona o elemento ao inicio da array
 //cart.sort() // Ele organiza os elementos em ordem alfabetica
-console.log(cart) // Descobrir o tamanho da array
+let y
+
+//y = cart.includes('maca') // Verifica se elemento existe no array
+//y = cart.indexOf('agua') // Verifica a posição do item no array
+
+//y = cart.slice(1, 3) //mostra os itens do array (de 0 até 1 nesse caso) ele não inclui o ultimo item
+y = cart.splice(1, 3) //mostra os itens do array mas inclui o ultimo item desejado. o Splice altera o estado do cart.
+// Ele retira do array os itens denominados
+console.log(y)
+console.log(cart)
