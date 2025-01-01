114b6d2fd026a1b24be0bc89c8c752c210c04722
Allison Boita <allison.boita75@gmail.com>
2025-01-01 08:33:58 -0300

    Validacoes com Operadores Lógicos

---
+++ Dia 14 - Operadores Lógicos/script.js
@@ -0,0 +1,24 @@
+// IF e ELSE com Operadores Logicos (&& E, || OU)
+
+/* // Criar um site de evento online.
+
+let idade = 18
+let registroOnline = true
+
+if (idade >= 18 && registroOnline) {
+  // Somente com o nome da variavel ele já considera como true
+  console.log('voce pode acessar')
+} else {
+  console.log('sem acesso')
+} */
+
+// APP onde o candidato recebe desconto se for estudante ou tiver cupom
+
+let estudante = false
+let cupom = true
+
+if (estudante || cupom) {
+  console.log('voce tem cupom de desconto!')
+} else {
+  console.log('voce nao tem desconto')
+}
