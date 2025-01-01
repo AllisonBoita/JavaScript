d6a9dc1c2b4dac44766832c55e52e9392d59e411
Allison Boita <allison.boita75@gmail.com>
2025-01-01 08:04:01 -0300

    Shorthand to IF e ELSE

--- Dia 13 - IF e ELSE/script.js
+++ Dia 13 - IF e ELSE/script.js
@@ -7,3 +7,19 @@ if (hour > 12) {
 } else {
   console.log('bom dia')
 }
+
+// Validado de cima pra baixo! executa o IF, se for falso avança para o ELSE IF. Se for verdadeiro, para por ali.
+
+// Utilizando o ELSE IF
+
+/* let hour = 18
+
+if (hour <= 12) {
+  console.log('boa dia')
+} else if (hour > 12 && hour <= 18) {
+  console.log('boa tarde')
+} else {
+  console.log('boa noite')
+} */
+
+// Variação de formato de IF e ELSE
