0d76238ee3d9f948b8f637325cb5179976478343
Allison Boita <allison.boita75@gmail.com>
2025-01-02 07:21:49 -0300

    Dia 17: Switch e Case

---
+++ Dia 17 - Switch e Case/exercise.js
@@ -0,0 +1,8 @@
+// Crie uma solução que verifica se o usuário escolheu "azul". caso seja outra cor utilize o not para retornar:
+// "Não temos essa cor"
+
+let escolhaCor = 'Amarelo'
+
+if (!(escolhaCor == 'Azul')) {
+  console.log('Não temos essa cor')
+}
---
+++ Dia 17 - Switch e Case/script.js
@@ -0,0 +1,37 @@
+// O Switch e Case
+
+// Dia 1 é domingo
+
+let dia = 3
+
+switch (
+  dia // expressao
+) {
+  case 1:
+    console.log('Domingo')
+    break
+  case 2:
+    console.log('Segunda')
+    break
+  case 3:
+    console.log('Terça')
+    break
+  case 4:
+    console.log('Quarta')
+    break
+  case 5:
+    console.log('Quinta')
+    break
+  case 6:
+    console.log('Sexta')
+    break
+  case 7:
+    console.log('Sabado')
+    break
+
+  default: // É tipo o ELSE
+    console.log('Numero Invalido')
+}
+
+// Comparação: Switch e CASE
+// Verificação: IF e ELSE
