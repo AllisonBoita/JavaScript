b6318ef63dcc370092c3424ec7c1622fd4786a0b
Allison Boita <allison.boita75@gmail.com>
2025-01-02 07:16:53 -0300

    Dia 16: Precedencia dos Operadores

---
+++ Dia 16 - Precedencia dos Operadores/script.js
@@ -0,0 +1,13 @@
+// Precedencia dos Operadores
+
+let resultado = 3 + 4 * 5
+console.log(resultado)
+
+let num1 = 5
+
+if (num1 == 5 || (num1 === 3 && num1 > 8)) {
+  console.log('Resultado Verdade')
+}
+
+// Nesse caso, a primeira parte é true e a parte do OU é false.
+// Como setei como OU ele retorna verdade.
