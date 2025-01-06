fa71dd7ae2fdff9af2b4fa2bc7563b2b554e3a30
Allison Cristiano Boita <allison.boita75@gmail.com>
2025-01-06 09:16:52 -0300

    Alterando e Acessando Objetos

--- Dia 19 - Objetos e Key Pairs/script.js
+++ Dia 19 - Objetos e Key Pairs/script.js
@@ -8,11 +8,14 @@ const ouraAllisonInfo = { // Dessa forma, com objects, você entende à qual inf
   totalSleep: 7.53,
   timeInBed: 9.7,
   sleepEfficiency: 86,
-  restingHeartRate: 59,
+  restingHeartRate: { // Como aninhar informações no objeto
+    minorRestingHeartRate: 59,
+    majorRestingHeartRate: 98
+  },
   sleepScore: 84
 }
 
-console.log(ouraAllisonInfo) // Acesso ao objeto completo
+/* console.log(ouraAllisonInfo) // Acesso ao objeto completo
 console.log(ouraAllisonInfo.sleepScore) // Acesso a algo específico no objeto
 console.log(ouraAllisonInfo['totalSleep']) // Outras formas de acesso
 
@@ -21,7 +24,8 @@ ouraAllisonInfo.remSleep = '2h35m' // Adicionando nova property
 ouraAllisonInfo.sleepScore = 90 // Alterando valor
 ouraAllisonInfo['sleepScore'] = 91 // Alterando valor
 ouraAllisonInfo.sleepScore++ // Alterando valor com operador
-ouraAllisonInfo['sleepScore'] += 4 // Alterando valor com operador
+ouraAllisonInfo['sleepScore'] += 4 // Alterando valor com operador */
 
+console.log(ouraAllisonInfo.restingHeartRate.majorRestingHeartRate) // acessando informação de objetos aninhados.
 console.log(ouraAllisonInfo) // Acesso ao objeto completo
 
