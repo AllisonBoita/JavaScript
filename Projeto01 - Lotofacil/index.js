1bc0e7e1a660058daaaf39847f4be77476d78c61
Allison Cristiano Boita <allison.boita75@gmail.com>
2025-01-10 08:28:40 -0300

    Ajustando projeto

---
+++ Projeto01 - Lotofacil/index.js
@@ -0,0 +1,23 @@
+const defaultUserNumbers = [2,4,5,7,9,10,11,13,14,15,16,20,22,24,25]
+
+    window.onload = function () {
+        document.getElementById("user-numbers").value = defaultUserNumbers;
+          }
+
+    function checkNumbers() {
+        const userNumbers = document.getElementById("user-numbers").value.split(",").map(Number);
+        const drawnNumbers = document.getElementById("drawn-numbers").value.split(",").map(Number);
+
+        const correctNumbers = userNumbers.filter(num => drawnNumbers.includes(num));
+        const resultDiv = document.getElementById("result");
+
+        if (correctNumbers.length >= 11){
+        resultDiv.innerHTML = `
+            <p class="correct-numbers"> Números Corretos: ${correctNumbers}</p>
+            <p class="total">Total de acertos: ${correctNumbers.length}</p>`
+        } else {
+            resultDiv.innerHTML =
+            `<p class="erro">Você não ganhou</p>`
+    }
+
+}
