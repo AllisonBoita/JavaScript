e0a440f8a8fe788e865c74303a3cfd05f50192ad
Allison Cristiano Boita <allison.boita75@gmail.com>
2025-01-10 17:55:45 -0300

    Operadores de Incremento

--- Javascript - Curso 2/Dia 02 - Operadores/Dia02.js
+++ Javascript - Curso 2/Dia 02 - Operadores/Dia02.js
@@ -1,4 +1,4 @@
-let num1 = 3
+let num1 = 1
 let num2 = 2
 
 let total = num1 + num2
@@ -6,3 +6,12 @@ let total = num1 + num2
 console.log(++total) // Se eu incrementar assim ele já soma no resultado
 console.log(total++) // Assim ele não soma, pq somou depois da exibição do total, tendo que ter mais um console.log
 console.log(total)
+
+///
+
+/// Se eu quiser somar mais
+
+num1 += 19
+num1 /= 20 // dividir...
+console.log(num1)
+
