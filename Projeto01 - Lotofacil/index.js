a8f7669028cc97d23905c28fb7ab7b6c0d641700
Allison Boita <allison.boita75@gmail.com>
2025-01-11 20:47:20 -0300

    Formatação com Prettier

--- Projeto01 - Lotofacil/index.js
+++ Projeto01 - Lotofacil/index.js
@@ -1,23 +1,29 @@
-const defaultUserNumbers = [2,4,5,7,9,10,11,13,14,15,16,20,22,24,25]
+const defaultUserNumbers = [
+  2, 4, 5, 7, 9, 10, 11, 13, 14, 15, 16, 20, 22, 24, 25,
+]
 
-    window.onload = function () {
-        document.getElementById("user-numbers").value = defaultUserNumbers;
-          }
+window.onload = function () {
+  document.getElementById('user-numbers').value = defaultUserNumbers
+}
 
-    function checkNumbers() {
-        const userNumbers = document.getElementById("user-numbers").value.split(",").map(Number);
-        const drawnNumbers = document.getElementById("drawn-numbers").value.split(",").map(Number);
+function checkNumbers() {
+  const userNumbers = document
+    .getElementById('user-numbers')
+    .value.split(',')
+    .map(Number)
+  const drawnNumbers = document
+    .getElementById('drawn-numbers')
+    .value.split(',')
+    .map(Number)
 
-        const correctNumbers = userNumbers.filter(num => drawnNumbers.includes(num));
-        const resultDiv = document.getElementById("result");
+  const correctNumbers = userNumbers.filter((num) => drawnNumbers.includes(num))
+  const resultDiv = document.getElementById('result')
 
-        if (correctNumbers.length >= 11){
-        resultDiv.innerHTML = `
+  if (correctNumbers.length >= 1) {
+    resultDiv.innerHTML = `
             <p class="correct-numbers"> Números Corretos: ${correctNumbers}</p>
             <p class="total">Total de acertos: ${correctNumbers.length}</p>`
-        } else {
-            resultDiv.innerHTML =
-            `<p class="erro">Você não ganhou</p>`
-    }
-
+  } else {
+    resultDiv.innerHTML = `<p class="erro">Você não ganhou</p>`
+  }
 }
