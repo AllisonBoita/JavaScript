312c9d961dbbab60b5ea69e32a095d0ffd35440b
Allison Cristiano Boita <allison.boita75@gmail.com>
2025-01-14 08:12:51 -0300

    Review: Objetos

---
+++ Javascript - Curso 2/Dia 03 - Object Oriented Programming/Dia03.js
@@ -0,0 +1,11 @@
+const book = {
+    name: 'Atomic Habits',
+    author: 'James Clear',
+    pages: 306,
+    bookChapters: {
+        chap1: 'Teste',
+        chap2: 'First Law'
+    }
+}
+
+console.log(book)
