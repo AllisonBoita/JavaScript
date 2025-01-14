cd22b26287d1ac6cd54445cbaa2819aa233be6fe
Allison Cristiano Boita <allison.boita75@gmail.com>
2025-01-14 08:22:46 -0300

    Factory

--- Javascript - Curso 2/Dia 03 - Object Oriented Programming/Dia03.js
+++ Javascript - Curso 2/Dia 03 - Object Oriented Programming/Dia03.js
@@ -17,7 +17,7 @@ console.log(book) */
 
 // Funções dentro de um objeto
 
-const book = {
+/* const book = {
     name: 'Atomic Habits',
     author: 'James Clear',
     pages: 306,
@@ -31,4 +31,31 @@ const book = {
 }
 
 book.printBook()
-console.log(book)
+console.log(book) */
+
+// Factory
+
+// Usado quando tenho dois objetos parecidos e quero colocar eles num grupo. (factory)
+
+// Cria objetos
+
+function createBooks (name, author, pages){ // factory
+    const book = {
+        name: name,
+        author: author,
+        pages: pages,
+        bookChapters: {
+            chap1: 'Teste',
+            chap2: 'First Law'
+        },
+        printBook: function () {
+            console.log('Printing...')
+        }
+    }
+    return book
+}
+
+const book1 = createBooks('Cem anos de Solidão', 'Gabriel Garcia Marques', 506)
+const book2 = createBooks('Chapeuzinho Vermelho', 'Gabriel Garcia Marques', 450)
+console.log(book1)
+console.log(book2)
