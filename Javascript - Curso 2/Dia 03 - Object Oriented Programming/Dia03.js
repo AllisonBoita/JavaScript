73d92ffb241a407e1be187f02a2c15b49df86b41
Allison Cristiano Boita <allison.boita75@gmail.com>
2025-01-14 08:16:06 -0300

    Funções dentro de um objeto

--- Javascript - Curso 2/Dia 03 - Object Oriented Programming/Dia03.js
+++ Javascript - Curso 2/Dia 03 - Object Oriented Programming/Dia03.js
@@ -1,4 +1,4 @@
-const book = {
+/* const book = {
     name: 'Atomic Habits',
     author: 'James Clear',
     pages: 306,
@@ -8,4 +8,27 @@ const book = {
     }
 }
 
+function printBook(){
+    console.log('Printing...')
+}
+
+printBook()
+console.log(book) */
+
+// Funções dentro de um objeto
+
+const book = {
+    name: 'Atomic Habits',
+    author: 'James Clear',
+    pages: 306,
+    bookChapters: {
+        chap1: 'Teste',
+        chap2: 'First Law'
+    },
+    printBook: function() {
+        console.log('Printing...')
+    }
+}
+
+book.printBook()
 console.log(book)
