73d2514f8eccc63fdbc0e6ef1e817bd46805185c
Allison Cristiano Boita <allison.boita75@gmail.com>
2025-01-17 08:26:09 -0300

    Localizando itens numa array de referência

--- Javascript - Curso 2/Dia 04 - Arrays/Dia04.js
+++ Javascript - Curso 2/Dia 04 - Arrays/Dia04.js
@@ -7,7 +7,21 @@ numeros.splice(2/*a partir de onde vou incluir*/, 0 /*quero deletar algo?*/, 4,5
 
 console.log(numeros)
 
-// Localizando arrays numa array
+// Localizando arrays numa array primitiva
 
 console.log(numeros.indexOf(9)) // verificando a localização
 console.log(numeros.includes(35)) // verificando se existe
+
+// Localizando itens numa array de referência
+
+const movies = [
+    {id: 1, movieName: 'Dejavu'},
+    {id: 2, movieName: 'Back to the future'},
+    {id: 3, movieName: 'The Matrix'}
+]
+
+console.log(movies.find(function (movies){
+    return movies.movieName == 'The Matrix'
+})) // retorna o filme se for igual ao que esotu passando.
+
+// Se for uma array de referência devemos utilizar o find
