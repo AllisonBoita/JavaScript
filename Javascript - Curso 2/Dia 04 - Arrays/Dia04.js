c43e3c5fde521ea8dd8c6433d52fd7e27d42f18d
Allison Cristiano Boita <allison.boita75@gmail.com>
2025-01-17 08:23:34 -0300

    Localizando arrays numa array

--- Javascript - Curso 2/Dia 04 - Arrays/Dia04.js
+++ Javascript - Curso 2/Dia 04 - Arrays/Dia04.js
@@ -1,4 +1,4 @@
-// Array
+// Array - Adicionando valores
 
 const numeros = [7,8,9,1,5]
 numeros./*acesso aos metodos*/push(10,11,12)
@@ -6,3 +6,8 @@ numeros.unshift(21,22) // Adicionando valores ao inicio do array
 numeros.splice(2/*a partir de onde vou incluir*/, 0 /*quero deletar algo?*/, 4,5,6)
 
 console.log(numeros)
+
+// Localizando arrays numa array
+
+console.log(numeros.indexOf(9)) // verificando a localização
+console.log(numeros.includes(35)) // verificando se existe
