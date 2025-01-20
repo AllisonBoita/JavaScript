3e6530281e49550502cedc5acd022ec7f46358f4
Allison Cristiano Boita <allison.boita75@gmail.com>
2025-01-20 07:32:48 -0300

    Concatenando array

---
+++ Javascript - Curso 2/Dia 04 - Arrays/Dia04pt2.js
@@ -0,0 +1,12 @@
+// Concatenando array
+
+let numbers = [2,3,4,5]
+let letters = ['a', 'b', 'c']
+
+all = numbers.concat(letters)
+half = all.slice(3,5)
+
+console.log(all)
+console.log(half)
+
+// Utilizando o Join Array
