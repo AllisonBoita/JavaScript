728f484ba8dcac274ebbc90ecfbcf7fcd61c68c2
Allison Cristiano Boita <allison.boita75@gmail.com>
2025-01-21 08:16:49 -0300

    Utilizando os Argumentos em funções

--- Javascript - Curso 2/Dia 05 - Functions/Dia05.js
+++ Javascript - Curso 2/Dia 05 - Functions/Dia05.js
@@ -1,4 +1,4 @@
-// Function declaration
+// Function Declaration
 
 function novaFuncao(){
     console.log('Filmes')
@@ -6,10 +6,22 @@ function novaFuncao(){
 
 novaFuncao()
 
-// Functio expression
+// Function Expression
 
 const car = function(){
     console.log('Tesla')
 } // Sem dar nome a ela
 
 car()
+
+// Utilizando os Argumentos em funções
+
+function somaValores(){
+    let total = 0
+    for (let i of arguments) // verifique os argumentos
+    // Quando eu uso o arguments não preciso definir os argumentos no inicio.
+        total += i
+    return total
+}
+
+console.log(somaValores(10,20,30,40))
