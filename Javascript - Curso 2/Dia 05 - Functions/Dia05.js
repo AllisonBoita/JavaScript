9d8c6cd9f0e1f14a88e391838d8a22b77b6b9fd2
Allison Cristiano Boita <allison.boita75@gmail.com>
2025-01-21 08:10:45 -0300

    Function Declaration e Expression

---
+++ Javascript - Curso 2/Dia 05 - Functions/Dia05.js
@@ -0,0 +1,15 @@
+// Function declaration
+
+function novaFuncao(){
+    console.log('Filmes')
+}
+
+novaFuncao()
+
+// Functio expression
+
+const car = function(){
+    console.log('Tesla')
+} // Sem dar nome a ela
+
+car()
