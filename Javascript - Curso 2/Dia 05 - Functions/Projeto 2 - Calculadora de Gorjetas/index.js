6f8285d4ae809696cf82a8345782ad82d745b217
Allison Cristiano Boita <allison.boita75@gmail.com>
2025-01-22 07:44:37 -0300

    Finalizando o projeto de Tips

--- Javascript - Curso 2/Dia 05 - Functions/Projeto 2 - Calculadora de Gorjetas/index.js
+++ Javascript - Curso 2/Dia 05 - Functions/Projeto 2 - Calculadora de Gorjetas/index.js
@@ -3,5 +3,13 @@ function update(){
     let tipPercent = document.getElementById('tipInput').value
     let split = document.getElementById('splitInput').value
 
-    console.log(bill,tipPercent,split)
+    let tipValue = bill * (tipPercent / 100)
+    let billTotal = bill + tipValue
+    let billEach = billTotal / split
+    
+    document.getElementById('tipPercent').innerHTML = tipPercent + ' %'
+    document.getElementById('tipValue').innerHTML = '$ ' + tipValue.toFixed(2)
+    document.getElementById('totalWithTip').innerHTML = '$ ' + billTotal.toFixed(2)
+    document.getElementById('splitValue').innerHTML = split
+    document.getElementById('billEach').innerHTML = '$ ' + billEach.toFixed(2)
 }
