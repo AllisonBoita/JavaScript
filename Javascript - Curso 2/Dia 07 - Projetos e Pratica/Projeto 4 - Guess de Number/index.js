e55af15b28c6139839c86899855f151f9c1f0294
Allison Cristiano Boita <allison.boita75@gmail.com>
2025-01-23 11:49:27 -0300

    Projeto Guess the Number: Adicionando Validações

--- Javascript - Curso 2/Dia 07 - Projetos e Pratica/Projeto 4 - Guess de Number/index.js
+++ Javascript - Curso 2/Dia 07 - Projetos e Pratica/Projeto 4 - Guess de Number/index.js
@@ -1,5 +1,23 @@
 let computerNumber
+let userNumbers = []
 
 function init(){
-    
+    computerNumber = Math.floor(Math.random() * 100 + 1)   // Gera um numero randomico e arredonda por causa do floor.
+}
+
+function registerNumbers(){
+    const userNumber = Number(document.getElementById('inputBox').value)
+    userNumbers.push(' ' + userNumber) // pegue os números e adicione à array userNumbers
+    document.getElementById('guesses').innerHTML = userNumbers
+
+    if (userNumber > computerNumber) {
+        document.getElementById('textOutput').innerHTML = 'Seu numero é maior que o número escolhido pelo computador'
+    } else if (userNumber < computerNumber){
+        document.getElementById('textOutput').innerHTML = 'Seu numero é menor que o número escolhido pelo computador'
+    } else {
+        document.getElementById('textOutput').innerHTML = 'Você acertou!'
+    }
+
+
+
 }
