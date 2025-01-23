8376fe8473b66f2092a4c5e0ccf9cd53f00f84ca
Allison Cristiano Boita <allison.boita75@gmail.com>
2025-01-23 16:24:08 -0300

    Adicionando validação de tentativas

--- Javascript - Curso 2/Dia 07 - Projetos e Pratica/Projeto 4 - Guess de Number/index.js
+++ Javascript - Curso 2/Dia 07 - Projetos e Pratica/Projeto 4 - Guess de Number/index.js
@@ -1,6 +1,7 @@
 let computerNumber
 let userNumbers = []
 //let attempts = 0
+let maxGuesses = 5
 
 function init(){
     computerNumber = Math.floor(Math.random() * 100 + 1)   // Gera um numero randomico e arredonda por causa do floor.
@@ -12,24 +13,32 @@ function registerNumbers(){
     userNumbers.push(' ' + userNumber) // pegue os números e adicione à array userNumbers
     document.getElementById('guesses').innerHTML = userNumbers
 
-    if (userNumber > computerNumber) {
-        document.getElementById('textOutput').innerHTML = 'Seu numero é maior que o número escolhido pelo computador'
-        document.getElementById('inputBox').value = ''
-        //attempts++
-        //document.getElementById('attempts').innerHTML = attempts
-        document.getElementById('attempts').innerHTML = userNumbers.length
-    } else if (userNumber < computerNumber){
-        document.getElementById('textOutput').innerHTML = 'Seu numero é menor que o número escolhido pelo computador'
-        document.getElementById('inputBox').value = ''
-        //attempts++
-        //document.getElementById('attempts').innerHTML = attempts
-        document.getElementById('attempts').innerHTML = userNumbers.length
+    if (userNumbers.length <= maxGuesses){
+
+        if (userNumber > computerNumber) {
+            document.getElementById('textOutput').innerHTML = 'Seu numero é maior que o número escolhido pelo computador'
+            document.getElementById('inputBox').value = ''
+            //attempts++
+            //document.getElementById('attempts').innerHTML = attempts
+            document.getElementById('attempts').innerHTML = userNumbers.length
+        } else if (userNumber < computerNumber){
+            document.getElementById('textOutput').innerHTML = 'Seu numero é menor que o número escolhido pelo computador'
+            document.getElementById('inputBox').value = ''
+            //attempts++
+            //document.getElementById('attempts').innerHTML = attempts
+            document.getElementById('attempts').innerHTML = userNumbers.length
+        } else {
+            document.getElementById('textOutput').innerHTML = 'Você acertou!'
+            //attempts++
+            //document.getElementById('attempts').innerHTML = attempts
+            document.getElementById('attempts').innerHTML = userNumbers.length
+        }
     } else {
-        document.getElementById('textOutput').innerHTML = 'Você acertou!'
-        //attempts++
-        //document.getElementById('attempts').innerHTML = attempts
-        document.getElementById('attempts').innerHTML = userNumbers.length
+        document.getElementById('textOutput').innerHTML = 'Você Perdeu!' + 'O número do computador era ' + computerNumber
+        return;
     }
+
+    
 }
 
 // Na aula foi repassada a forma de incrementar a tentativa de attempts em todos os ifs, porém notei que da forma feita em código o resultado é o mesmo.
