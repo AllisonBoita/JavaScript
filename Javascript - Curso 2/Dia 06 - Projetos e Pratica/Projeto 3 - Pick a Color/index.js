03bb18b590e21ed8d678bb43f96b9410f7e60bc6
Allison Cristiano Boita <allison.boita75@gmail.com>
2025-01-23 10:24:36 -0300

    Alterando cor de background

--- Javascript - Curso 2/Dia 06 - Projetos e Pratica/Projeto 3 - Pick a Color/index.js
+++ Javascript - Curso 2/Dia 06 - Projetos e Pratica/Projeto 3 - Pick a Color/index.js
@@ -1,2 +1,9 @@
 const cores = ['Red', 'Blue', 'Yellow', 'Purple', 'Gray', 'Green']
 const btn = document.getElementById('btn')
+
+// Escutar um clique no botão do index.html
+
+btn.addEventListener('click', function(){
+    // Pegar um iten da minha array e colocar como background do body html
+    document.body.style.background = cores[1] // vai do documento e troca o background para...
+})
