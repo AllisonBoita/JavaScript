738c1dc966b95fcee5cf36a4c0a1cccc2803553c
Allison Cristiano Boita <allison.boita75@gmail.com>
2025-01-23 13:34:15 -0300

    Ajustando o número de tentativas

--- Javascript - Curso 2/Dia 07 - Projetos e Pratica/Projeto 4 - Guess de Number/index.js
+++ Javascript - Curso 2/Dia 07 - Projetos e Pratica/Projeto 4 - Guess de Number/index.js
@@ -1,8 +1,10 @@
 let computerNumber
 let userNumbers = []
+//let attempts = 0
 
 function init(){
     computerNumber = Math.floor(Math.random() * 100 + 1)   // Gera um numero randomico e arredonda por causa do floor.
+    //console.log(computerNumber)
 }
 
 function registerNumbers(){
@@ -12,12 +14,22 @@ function registerNumbers(){
 
     if (userNumber > computerNumber) {
         document.getElementById('textOutput').innerHTML = 'Seu numero é maior que o número escolhido pelo computador'
+        document.getElementById('inputBox').value = ''
+        //attempts++
+        //document.getElementById('attempts').innerHTML = attempts
+        document.getElementById('attempts').innerHTML = userNumbers.length
     } else if (userNumber < computerNumber){
         document.getElementById('textOutput').innerHTML = 'Seu numero é menor que o número escolhido pelo computador'
+        document.getElementById('inputBox').value = ''
+        //attempts++
+        //document.getElementById('attempts').innerHTML = attempts
+        document.getElementById('attempts').innerHTML = userNumbers.length
     } else {
         document.getElementById('textOutput').innerHTML = 'Você acertou!'
+        //attempts++
+        //document.getElementById('attempts').innerHTML = attempts
+        document.getElementById('attempts').innerHTML = userNumbers.length
     }
-
-
-
 }
+
+// Na aula foi repassada a forma de incrementar a tentativa de attempts em todos os ifs, porém notei que da forma feita em código o resultado é o mesmo.
