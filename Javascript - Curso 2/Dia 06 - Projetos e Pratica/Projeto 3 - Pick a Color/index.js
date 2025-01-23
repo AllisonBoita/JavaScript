d06b662deff3ec1a72da3a5753d6b20c107c9b97
Allison Cristiano Boita <allison.boita75@gmail.com>
2025-01-23 11:16:42 -0300

    Ajustando projeto para cores aleatórias

--- Javascript - Curso 2/Dia 06 - Projetos e Pratica/Projeto 3 - Pick a Color/index.js
+++ Javascript - Curso 2/Dia 06 - Projetos e Pratica/Projeto 3 - Pick a Color/index.js
@@ -3,7 +3,22 @@ const btn = document.getElementById('btn')
 
 // Escutar um clique no botão do index.html
 
-btn.addEventListener('click', function(){
-    // Pegar um iten da minha array e colocar como background do body html
-    document.body.style.background = cores[1] // vai do documento e troca o background para...
+/* btn.addEventListener('click', function(){
+    // Utilizando o Math.floor para aredondar o array para um inteiro.
+    const indiceAleatorio = Math.floor(Math.random() * cores.length) // Multiplicamos o número aleatório pelo número total de cores no seu array. Isso nos dá um número aleatório entre 0 e o número de cores (mas ainda com casas decimais).
+    const corAleatoria = cores[indiceAleatorio] // Usamos o índice aleatório para acessar uma cor específica do array.
+
+    document.body.style.background = corAleatoria // vai do documento e troca o background para...
 })
+ */
+
+btn.addEventListener('click', function () {
+    const randomColor = getRandomColor()
+    const corAleatoria = cores[randomColor]
+    document.body.style.background = corAleatoria
+    // console.log(randomColor)
+})
+
+function getRandomColor (){
+    return Math.floor(Math.random() * cores.length)
+}
